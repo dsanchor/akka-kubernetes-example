5451555829db3225757d7a98a8e014a4860e951e
David Sancho <dsanchor@redhat.com>
2017-04-27 17:54:39 +0200

    Different test for remote

--- hello-world/src/main/java/com/redhat/services/akka/HelloWorld.java
+++ hello-world/src/main/java/com/redhat/services/akka/HelloWorld.java
@@ -1,17 +1,35 @@
 package com.redhat.services.akka;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorIdentity;
+import akka.actor.ReceiveTimeout;
+import akka.actor.UntypedActor;
+import akka.cluster.ClusterEvent.MemberUp;
 
-public class HelloWorld extends AbstractActor {
+public class HelloWorld extends UntypedActor {
 	
-	  @Override
-	  public Receive createReceive() {
-	    return receiveBuilder()
-	      .match(String.class, m -> {
-	    	  String message = "Hello " + m.toUpperCase();
-	          System.out.println(message);
-	          sender().tell(message, self());
-	      })
-	      .build();
-	  }
+	@Override
+	public void onReceive(Object message) throws Exception {
+		if (message instanceof String) {
+			String m = "Hello " + message;
+			System.out.println(m);
+			sender().tell(m, self());
+
+		} else {
+			unhandled(message);
+
+		}
+	}
+	
+	
+//	  @Override
+//	  public Receive createReceive() {
+//	    return receiveBuilder()
+//	      .match(String.class, m -> {
+//	    	  String message = "Hello " + m.toUpperCase();
+//	          System.out.println(message);
+//	          sender().tell(message, self());
+//	      })
+//	      .build();
+//	  }
 }
