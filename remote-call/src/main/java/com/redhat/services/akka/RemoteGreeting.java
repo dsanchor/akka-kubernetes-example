c9f83c87b06a006d6ca217ea42227ead63e0e7dc
David Sancho <dsanchor@redhat.com>
2017-04-27 13:17:46 +0200

    Different test for remote

--- remote-call/src/main/java/com/redhat/services/akka/RemoteGreeting.java
+++ remote-call/src/main/java/com/redhat/services/akka/RemoteGreeting.java
@@ -1,74 +1,56 @@
 package com.redhat.services.akka;
 
-import java.util.concurrent.TimeUnit;
-
-import akka.actor.ActorIdentity;
 import akka.actor.ActorRef;
-import akka.actor.Identify;
-import akka.actor.ReceiveTimeout;
-import akka.actor.Terminated;
 import akka.actor.UntypedActor;
-import akka.japi.Procedure;
-import scala.concurrent.duration.Duration;
+import akka.cluster.Cluster;
+import akka.cluster.ClusterEvent;
+import akka.cluster.ClusterEvent.MemberEvent;
+import akka.cluster.ClusterEvent.MemberUp;
+import akka.cluster.ClusterEvent.UnreachableMember;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 
 public class RemoteGreeting extends UntypedActor {
-	
+
+	LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
 	private final String path;
-	  private ActorRef helloWorld = null;
+	private ActorRef helloWorld = null;
 
-	  public RemoteGreeting(String path) {
-	    this.path = path;
-	    sendIdentifyRequest();
-	  }
+	Cluster cluster = Cluster.get(getContext().system());
 
-	  private void sendIdentifyRequest() {
-	    getContext().actorSelection(path).tell(new Identify(path), getSelf());
-	    getContext()
-	        .system()
-	        .scheduler()
-	        .scheduleOnce(Duration.create(3, TimeUnit.SECONDS), getSelf(),
-	            ReceiveTimeout.getInstance(), getContext().dispatcher(), getSelf());
-	  }
+	public RemoteGreeting(String path) {
+		this.path = path;
+	}
 
-	  @Override
-	  public void onReceive(Object message) throws Exception {
-	    if (message instanceof ActorIdentity) {
-	    	helloWorld = ((ActorIdentity) message).getRef();
-	      if (helloWorld == null) {
-	        System.out.println("Remote actor not available: " + path);
-	      } else {
-	        getContext().watch(helloWorld);
-	        getContext().become(active, true);
-	      }
+	// subscribe to cluster changes
+	@Override
+	public void preStart() {
+		// #subscribe
+		cluster.subscribe(getSelf(), ClusterEvent.initialStateAsEvents(), MemberEvent.class, UnreachableMember.class);
+		// #subscribe
+	}
 
-	    } else if (message instanceof ReceiveTimeout) {
-	      sendIdentifyRequest();
+	// re-subscribe when restart
+	@Override
+	public void postStop() {
+		cluster.unsubscribe(getSelf());
+	}
 
-	    } else {
-	      System.out.println("Not ready yet");
+	@Override
+	public void onReceive(Object message) {
+		if (message instanceof MemberUp) {
+			MemberUp mUp = (MemberUp) message;
+			log.info("Member is Up: {}", mUp.member());
+			 getContext().actorSelection(path).tell(mUp.member().address().hostPort(),getSelf());
 
-	    }
-	  }
+		} else if (message instanceof MemberEvent) {
+			// ignore
+		} else if (message instanceof String) {
+			log.info((String)message);
 
-	  Procedure<Object> active = new Procedure<Object>() {
-	    @Override
-	    public void apply(Object message) {
-	      if (message instanceof String) {
-	        // send message to server actor
-	        helloWorld.tell(message, getSelf());
-
-	      } else if (message instanceof Terminated) {
-	        System.out.println("HelloWorld terminated");
-	        sendIdentifyRequest();
-	        getContext().unbecome();
-
-	      } else if (message instanceof ReceiveTimeout) {
-	        // ignore
-
-	      } else {
-	        unhandled(message);
-	      }
-
-	    }
-	  };
+		} else {
+			unhandled(message);
+		}
+	}
 }
--- remote-call/src/main/java/com/redhat/services/akka/RemoteMain.java
+++ remote-call/src/main/java/com/redhat/services/akka/RemoteMain.java
@@ -1,40 +1,22 @@
 package com.redhat.services.akka;
 
 import java.io.IOException;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import scala.concurrent.duration.Duration;
 
 public class RemoteMain {
-  private static final String CLUSTER_NAME = "RemoteGreetingSystem";
-  
-  private static final String REMOTE_PATH = "REMOTE_SERVICE_PATH";
-  
-  private static final String[] names = new String[]{"David", "Michael", "Alex", "George", "Jose"};
+	private static final String CLUSTER_NAME = "RemoteGreetingSystem";
 
-  public static void main(String[] args) throws IOException {
-    ActorSystem actorSystem = ActorSystem.create(CLUSTER_NAME);
-    final ActorRef actor = actorSystem.actorOf(
-        Props.create(RemoteGreeting.class, getRemotePath()), "remoteGreeting");
+	private static final String REMOTE_PATH = "REMOTE_SERVICE_PATH";
 
-        System.out.println("Started RemoteGreetingSystem");
-        final Random r = new Random();
-        actorSystem.scheduler().schedule(Duration.create(1, TimeUnit.SECONDS),
-            Duration.create(1, TimeUnit.SECONDS), new Runnable() {
-              @Override
-              public void run() {
-            	  int index = new Random().nextInt(5);
-                  actor.tell(names[index], null);
+	public static void main(String[] args) throws IOException {
+		ActorSystem actorSystem = ActorSystem.create(CLUSTER_NAME);
+		actorSystem.actorOf(Props.create(RemoteGreeting.class, getRemotePath()), "remoteGreeting");
+		System.out.println("Started RemoteGreetingSystem");
+	}
 
-              }
-            }, actorSystem.dispatcher());
-  }
-  
-  private static String getRemotePath() {
-	  return System.getenv(REMOTE_PATH);
-  }
+	private static String getRemotePath() {
+		return System.getenv(REMOTE_PATH);
+	}
 }
