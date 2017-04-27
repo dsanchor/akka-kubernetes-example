1d0f487c924e52ccf8e86e0bbd57ed86ff71bd56
David Sancho <dsanchor@redhat.com>
2017-04-27 17:00:24 +0200

    Different test for remote

--- remote-call/src/main/java/com/redhat/services/akka/RemoteGreeting.java
+++ remote-call/src/main/java/com/redhat/services/akka/RemoteGreeting.java
@@ -1,7 +1,12 @@
 package com.redhat.services.akka;
 
+import java.util.concurrent.TimeUnit;
+
+import akka.actor.ActorIdentity;
 import akka.actor.ActorRef;
-import akka.actor.ActorSelection;
+import akka.actor.Identify;
+import akka.actor.ReceiveTimeout;
+import akka.actor.Terminated;
 import akka.actor.UntypedActor;
 import akka.cluster.Cluster;
 import akka.cluster.ClusterEvent;
@@ -10,6 +15,8 @@ import akka.cluster.ClusterEvent.MemberUp;
 import akka.cluster.ClusterEvent.UnreachableMember;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import akka.japi.Procedure;
+import scala.concurrent.duration.Duration;
 
 public class RemoteGreeting extends UntypedActor {
 
@@ -22,7 +29,27 @@ public class RemoteGreeting extends UntypedActor {
 
 	public RemoteGreeting(String path) {
 		this.path = path;
-		// sendIdentifyRequest();
+		sendIdentifyRequest();
+	}
+
+	// subscribe to cluster changes
+	@Override
+	public void preStart() {
+		// #subscribe
+		cluster.subscribe(getSelf(), ClusterEvent.initialStateAsEvents(), MemberEvent.class, UnreachableMember.class);
+		// #subscribe
+	}
+
+	// re-subscribe when restart
+	@Override
+	public void postStop() {
+		cluster.unsubscribe(getSelf());
+	}
+
+	private void sendIdentifyRequest() {
+		getContext().actorSelection(path).tell(new Identify(path), getSelf());
+		getContext().system().scheduler().scheduleOnce(Duration.create(3, TimeUnit.SECONDS), getSelf(),
+				ReceiveTimeout.getInstance(), getContext().dispatcher(), getSelf());
 	}
 
 	@Override
@@ -30,81 +57,43 @@ public class RemoteGreeting extends UntypedActor {
 		if (message instanceof MemberUp) {
 			MemberUp mUp = (MemberUp) message;
 			log.info("Member is Up: {}", mUp.member());
-			ActorSelection actorSelection = getContext().actorSelection(path);
-			helloWorld = actorSelection.anchor();
 			helloWorld.tell(mUp.member().address().hostPort(), getSelf());
-		} else if (message instanceof String) {
-			log.info("Message received is {}", message);
+
+		} else if (message instanceof ActorIdentity) {
+			helloWorld = ((ActorIdentity) message).getRef();
+			if (helloWorld == null) {
+				System.out.println("Remote actor not available: " + path);
+			} else {
+				getContext().watch(helloWorld);
+				getContext().become(active, true);
+			}
+
+		} else if (message instanceof ReceiveTimeout) {
+			sendIdentifyRequest();
+
 		} else {
-		      unhandled(message);
+			System.out.println("Not ready yet");
+
 		}
 	}
 
-	 // subscribe to cluster changes
-	 @Override
-	 public void preStart() {
-	 // #subscribe
-	 cluster.subscribe(getSelf(), ClusterEvent.initialStateAsEvents(),
-	 MemberEvent.class, UnreachableMember.class);
-	 // #subscribe
-	 }
-	
-	 // re-subscribe when restart
-	 @Override
-	 public void postStop() {
-	 cluster.unsubscribe(getSelf());
-	 }
-	 
-	//
-	// private void sendIdentifyRequest() {
-	// getContext().actorSelection(path).tell(new Identify(path), getSelf());
-	// getContext().system().scheduler().scheduleOnce(Duration.create(3,
-	// TimeUnit.SECONDS), getSelf(),
-	// ReceiveTimeout.getInstance(), getContext().dispatcher(), getSelf());
-	// }
-	//
-	// @Override
-	// public void onReceive(Object message) throws Exception {
-	// if (message instanceof MemberUp) {
-	// MemberUp mUp = (MemberUp) message;
-	// log.info("Member is Up: {}", mUp.member());
-	// helloWorld.tell(mUp.member().address().hostPort(), getSelf());
-	//
-	// } else if (message instanceof ActorIdentity) {
-	// helloWorld = ((ActorIdentity) message).getRef();
-	// if (helloWorld == null) {
-	// System.out.println("Remote actor not available: " + path);
-	// } else {
-	// getContext().watch(helloWorld);
-	// getContext().become(active, true);
-	// }
-	//
-	// } else if (message instanceof ReceiveTimeout) {
-	// sendIdentifyRequest();
-	//
-	// } else {
-	// System.out.println("Not ready yet");
-	//
-	// }
-	// }
-	//
-	// Procedure<Object> active = new Procedure<Object>() {
-	// @Override
-	// public void apply(Object message) {
-	// if (message instanceof Terminated) {
-	// System.out.println("HelloWorld terminated");
-	// sendIdentifyRequest();
-	// getContext().unbecome();
-	//
-	// } else if (message instanceof ReceiveTimeout) {
-	// // ignore
-	// } else if (message instanceof String) {
-	// log.info((String) message);
-	//
-	// } else {
-	// unhandled(message);
-	// }
-	//
-	// }
-	// };
+	Procedure<Object> active = new Procedure<Object>() {
+		@Override
+		public void apply(Object message) {
+			 if (message instanceof Terminated) {
+				System.out.println("HelloWorld terminated");
+				sendIdentifyRequest();
+				getContext().unbecome();
+
+			} else if (message instanceof ReceiveTimeout) {
+				// ignore
+			} else if (message instanceof String) {
+				log.info("Message received: {}", (String) message);
+
+			} else {
+				unhandled(message);
+			}
+
+		}
+	};
 }
