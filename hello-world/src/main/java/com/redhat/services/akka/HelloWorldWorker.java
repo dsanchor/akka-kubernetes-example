bf70ace2d8017186532fa1ea10c1364f63ba0717
David Sancho <dsanchor@redhat.com>
2017-05-02 13:18:54 +0200

    Different test for remote

--- hello-world/src/main/java/com/redhat/services/akka/HelloWorldWorker.java
+++ hello-world/src/main/java/com/redhat/services/akka/HelloWorldWorker.java
@@ -3,7 +3,7 @@ package com.redhat.services.akka;
 import akka.actor.UntypedActor;
 
 public class HelloWorldWorker extends UntypedActor {
-	
+
 	@Override
 	public void onReceive(Object message) throws Exception {
 		if (message instanceof String) {
@@ -16,16 +16,15 @@ public class HelloWorldWorker extends UntypedActor {
 
 		}
 	}
-	
-	
-//	  @Override
-//	  public Receive createReceive() {
-//	    return receiveBuilder()
-//	      .match(String.class, m -> {
-//	    	  String message = "Hello " + m.toUpperCase();
-//	          System.out.println(message);
-//	          sender().tell(message, self());
-//	      })
-//	      .build();
-//	  }
+
+	// @Override
+	// public Receive createReceive() {
+	// return receiveBuilder()
+	// .match(String.class, m -> {
+	// String message = "Hello " + m.toUpperCase();
+	// System.out.println(message);
+	// sender().tell(message, self());
+	// })
+	// .build();
+	// }
 }
